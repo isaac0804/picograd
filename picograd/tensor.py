ce733be7c6bc3db38508df56ecff6284733dd389
Isaac Yong <isaacyong0804@gmail.com>
2022-08-18 11:56:33 +0800

    Basic grad

--- picograd/tensor.py
+++ picograd/tensor.py
@@ -1,18 +1,92 @@
 
 class Tensor:
-    def __init__(self, data):
+    def __init__(self, data, _children=()):
         self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._children = set(_children)
+
         # TODO: Shape
         # self.shape = shape
+
+    def backward(self):
+        nodes = []
+
+        def build_graph(x):
+            if x not in nodes:
+                for c in x._children:
+                    build_graph(c)
+                nodes.append(x)
+        build_graph(self)
+
+        self.grad = 1.0
+        for node in reversed(nodes):
+            node._backward()
+        
+        for node in nodes:
+            print(node, node.grad)
 
     def __repr__(self):
         return f"Tensor with data {self.data}"
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data + other.data, (self, other))
+
+        def _backward():
+            """
+            L = f(x)
+            x = a+b
+            dL/da = dL/dx * dx/da = dL/dx
+            dL/db = dL/dx * dx/db = dL/dx
+            """
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
 
     def __mul__(self, other):
-        return Tensor(self.data * other.data)
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data * other.data, (self, other))
+
+        def _backward():
+            """
+            L = f(x)
+            x = a*b
+            dL/da = dL/dx * dx/da = dL/dx * b
+            dL/db = dL/dx * dx/db = dL/dx * a
+            """
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+
+    def __pow__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data ** other.data, (self, other))
+
+        def _backward():
+            # Assume b is a constant
+            """
+            L = f(x)
+            x = a**b
+            dL/da = dL/dx * b * a ** (b - 1)
+            """
+            self.grad += other.data * self.data ** (other.data - 1) * out.grad
+        out._backward = _backward
+        return out
 
     def __rmul__(self, other):
         return self * other
+
+    def __neg__(self):
+        return self * -1.0
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __truediv__(self, other):  # self / other
+        return self * (1.0 / other)
+
+    def __rtruediv__(self, other):  # other / self
+        return other * (1.0 / self)
--- test.py
+++ test.py
@@ -1,16 +1,28 @@
 from picograd.tensor import Tensor
 
 def main():
-    a = Tensor(1.0)
-    b = Tensor(2.0)
-    print(a)
-    print(b)
+    
+    a = Tensor(3.0)
+    b = Tensor(4.0)
 
-    c = a+b
-    print(c)
+    def forward(a, b):
+        c = a+b
+        d = a*b + c
+        e = d**2
+        L = e
+        return L
 
-    d = a*b
-    print(d)
+    L = forward(a, b)
+    L.backward()
+    print(b.grad)
+
+    def test_grad():
+        h = 1e-6
+        grad = (forward(a, b+h) - forward(a, b)) / h
+        print(grad)
+    
+    test_grad()
+
 
 if __name__ == '__main__':
     main()
