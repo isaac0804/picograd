413799617723f4a07e01284a98afe6d40b9ba443
Isaac Yong <isaacyong0804@gmail.com>
2022-08-18 12:25:45 +0800

    Add test and reference, not passing the test

--- picograd/tensor.py
+++ picograd/tensor.py
@@ -25,6 +25,19 @@ class Tensor:
         
         for node in nodes:
             print(node, node.grad)
+    
+    def relu(self):
+        out = Tensor(self.data * (self.data > 0), (self,))
+
+        def _backward():
+            """
+            L = f(x)
+            x = relu(a)
+            dL/da = dL/dx * dx/da
+            """
+            self.grad += (out.data > 0) * out.data * out.grad
+        out._backward = _backward
+        return out
 
     def __repr__(self):
         return f"Tensor with data {self.data}"
@@ -75,6 +88,9 @@ class Tensor:
             self.grad += other.data * self.data ** (other.data - 1) * out.grad
         out._backward = _backward
         return out
+    
+    def __radd__(self, other):
+        return self + other
 
     def __rmul__(self, other):
         return self * other
@@ -86,7 +102,19 @@ class Tensor:
         return self + (-other)
 
     def __truediv__(self, other):  # self / other
-        return self * (1.0 / other)
+        return self * other ** -1.0
 
     def __rtruediv__(self, other):  # other / self
-        return other * (1.0 / self)
+        return (self / other) ** -1.0
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __isub__(self, other):
+        return self - other
+
+    def __imul__(self, other):
+        return self * other
+
+    def __idiv__(self, other):
+        return self / other
